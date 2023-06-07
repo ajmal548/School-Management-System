cfa97fb2dd9e45536fcc844753a4fa752b811bf3
ajmal548 <ajmalknlr2001@gmail.com>
2023-06-07 12:06:53 +0530

    edit

--- routes/exam.js
+++ routes/exam.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 var exam = require("../schema/examschema");
 
-router.post("/",async(req,res)=>{
+router.post("/", async (req, res) => {
     var newexam = new exam({
-        examination:req.body.examination
+        examination: req.body.examination
     });
-        var data= await newexam.save();
-        res.send(data);
+    var data = await newexam.save();
+    res.send(data);
 });
-module.exports=router;
+module.exports = router;
