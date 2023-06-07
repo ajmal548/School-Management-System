4568382658ac03d1f27f931ee749a61e4ae743ba
ajmal548 <ajmalknlr2001@gmail.com>
2023-06-07 12:02:28 +0530

    edit

--- schema/studentschema.js
+++ schema/studentschema.js
@@ -2,12 +2,8 @@ var mongoose = require('mongoose');
 var studentsSchema = mongoose.Schema({
   name: String,
   age: Number,
-  ph: Number,
-  
+  ph: Number,  
 });
-
-
-
 var studentmodel = mongoose.model("student", studentsSchema, "students");
 
 module.exports = studentmodel;
